3245f52a46a4a33edf81b32bee31d623622f7f36
kolisksham <152739447+kolisksham@users.noreply.github.com>
2025-08-04 04:44:58 +0530

    Update 2loops.py

--- sec_1/2loops.py
+++ sec_1/2loops.py
@@ -42,3 +42,68 @@ for i in range(1, 11):
     else:
         print(i)
 print("_" * 100, "\n")
+
+#Question 5: Sum of Multiples (for loop)
+# Write a for loop that finds the sum of all multiples of 3 
+# between 1 and 30 (inclusive). Print the final sum.
+
+print("5. Sum of multiples of 3 from 1 to 30:")
+sum_multiples = 0
+for i in range(1, 31):
+    if i % 3 == 0:
+        sum_multiples += i
+print("Total sum is:", sum_multiples)
+print("_" * 100, "\n")
+
+# Question 6: Number Guessing (while loop + input)
+# Write a while loop that keeps asking the user to guess a number.
+# If the guess is correct, print "Correct!" and exit the loop.
+# If the guess is wrong, print "Try again!" and loop again.
+
+print("Guessing Game: ")
+
+num = 7
+guess = 0
+
+while(guess != num):
+    guess = int(input("Enter your choice(1-10): "))
+    if(guess < num):
+        print("Try a greater number. ")
+        continue
+    elif(guess > num):
+        print("Try a smaller number. ")
+        continue
+    else:
+        print("You Won!")
+        break
+print("_" * 100, "\n")
+
+# Question 7: Print a Square Pattern (nested loops)
+# Print a square of size 4x4 using "*". Your output should look exactly like:
+# * * * *
+# * * * *
+# * * * *
+# * * * *
+
+print("7. Rectangle pattern: ")
+l = int(input("Enter length: "))
+b = int(input("Enter breadth: "))
+
+for i in range(l):
+    for j in range(b):
+        print("*", end=" ")
+    print()
+print("_" * 100, "\n")
+
+# Question 8: Skip Odd Numbers (continue in for loop)
+# Write a for loop that prints only the even numbers from 1 to 10 (inclusive). 
+# Use continue to skip odd numbers.
+
+print("8. Skipping Odd numbers: ")
+ul = int(input("Enter upper limit: ")) #we're taking a custom number
+for i in range(1, ul+1):
+    if(i % 2 != 0):
+        continue
+    else:
+        print(i)
+print("_" * 100, "\n")
