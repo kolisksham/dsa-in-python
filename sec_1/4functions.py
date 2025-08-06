0b09407e84cef7045303b2970cffedc4845c766c
kolisksham <152739447+kolisksham@users.noreply.github.com>
2025-08-06 22:49:43 +0530

    Create 4functions.py

---
+++ sec_1/4functions.py
@@ -0,0 +1,14 @@
+def greet():
+    print("Hello from a function!")
+
+greet()
+
+def greet(name):
+    print("Hello,", name)
+greet("Saksham")  # Output: Hello, Alice
+
+def add(a, b):
+    return a + b
+
+result = add(3, 4)  # result is 7
+print(result)
